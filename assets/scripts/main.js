173f628a946e539fcec87a017db07dc66b39acc7
deyems <adeyemisola11@gmail.com>
2020-12-13 16:49:01 +0100

    added slideshow, animation of text and their scripts

---
+++ assets/scripts/main.js
@@ -0,0 +1,124 @@
+//Do  Slide show Here
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("testifier-text-content");
+  let dots = document.getElementsByClassName("dots");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    //   slides[i].style.marginRight = "-100%";
+
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+//   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//Script to Animate Text
+// Wrap every letter in a span
+// var textWrapper = document.querySelector('.ml11 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml11 .line',
+//     scaleY: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 700
+//   })
+//   .add({
+//     targets: '.ml11 .line',
+//     translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     delay: 100
+//   })
+//   .add({
+//     targets: '.ml11 .letter',
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 2000,
+//     offset: '-=775',
+//     delay: (el, i) => 34 * (i+1)
+//   }).add({
+//     targets: '.ml11',
+//     opacity: 0,
+//     duration: 700,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
+//ANother script to animate second line in the header
+  //Text Coming Out Gradually
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    // var css = document.createElement("style");
+    // css.type = "text/css";
+    // css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    // document.body.appendChild(css);
+  };
